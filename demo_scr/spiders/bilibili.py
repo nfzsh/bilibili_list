34172f4b43d2055065fef95824fc0b6f55fd722f
nfzsh <zsh199803@hotmail.com>
2020-11-04 15:02:26 +0800

    同步网站更新，获取同up主热榜视频信息，更改rank值为热度值，取消排名值（由于网站更新已事实无效）

--- demo_scr/spiders/bilibili.py
+++ demo_scr/spiders/bilibili.py
@@ -3,6 +3,7 @@ from bilibili_api import video
 from ..items import BilibiliListScrItem
 import time
 
+
 class BilibiliSpider(scrapy.Spider):
     name = 'bilibili'
     allowed_domains = ['www.bilibili.com']
@@ -14,11 +15,28 @@ class BilibiliSpider(scrapy.Spider):
         for i in lists:
             items['date'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             items['type'] = 'all'
-            items['rank'] = i.xpath('./div[1]/text()').get()
+            items['rank'] = i.xpath('./div[2]/div[2]/div[2]/div/text()').get()
             items['url'] = i.xpath('./div[2]/div[1]/a/@href').get()[2:]
             # items['pic'] = i.xpath('./div[2]/div[1]/a/img/@data-src').get()
             items['name'] = i.xpath('./div[2]/div[2]/a/text()').get()
             items['bv'] = i.xpath('./div[2]/div[1]/a/@href').get()[25:]
+            v = video.get_video_info(bvid=items['bv'])
+            items['pic'] = v['pic']
+            items['aid'] = v['aid']
+            items['view'] = v['stat']['view']
+            items['up'] = v['owner']['name']
+            items['upId'] = v['owner']['mid']
+            yield items
+        #     补充更新同up主多数据爬取
+        lists = response.xpath('//*[@id="app"]/div[2]/div[2]/ul/li/div[2]/div[3]/div/a')
+        for i in lists:
+            items['date'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            items['type'] = 'all'
+            items['rank'] = i.xpath('./strong/text()').get()
+            items['url'] = i.xpath('./@href').get()[2:]
+            # items['pic'] = i.xpath('./div[2]/div[1]/a/img/@data-src').get()
+            items['name'] = i.xpath('./span/text()').get()
+            items['bv'] = i.xpath('./@href').get()[25:]
             v = video.get_video_info(bvid=items['bv'])
             items['pic'] = v['pic']
             items['aid'] = v['aid']
